ef1b3f206bcbd6c8c2fc200db3dabc1cbccf9b64
IteraDynamics <andrew@moonwire.app>
2026-09-17 14:05:32 -0400

    test: cover conditional destination filtering

--- tests/test_latent_transition_geometry.py
+++ tests/test_latent_transition_geometry.py
@@ -1,6 +1,5 @@
-import numpy as np
 import pandas as pd
-from research.latent_transition_geometry.study import first_destination,HORIZONS
+from research.latent_transition_geometry.study import first_destination,HORIZONS,_multiclass
 
 def test_first_destination_uses_first_change():
  s=pd.Series(['A','A','B','C','C'])
@@ -15,3 +14,11 @@ def test_first_destination_marks_incomplete_horizon_missing():
 
 def test_horizons_are_frozen():
  assert HORIZONS==(1,2,3,5,7,10,14)
+
+def test_conditional_destination_excludes_no_transition():
+ train=pd.DataFrame({'x':[0.,1.,2.,3.,4.,5.],'destination':['NO_TRANSITION','A','B','A','B','NO_TRANSITION']})
+ test=pd.DataFrame({'x':[1.,2.,3.],'destination':['A','B','NO_TRANSITION']})
+ score=_multiclass(train,test,['x'],'destination',conditional_transition=True)
+ assert score is not None
+ assert score['observations']==2
+ assert score['classes']==2
